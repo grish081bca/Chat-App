0010f563082178d7a1de1a0c45637ce31647d22e
grish <grishshrestha2@gmail.com>
2025-07-14 22:10:00 +0545

    Add elements and styling

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+ var userNamePage = document.querySelector("#username-page");
+ var chatPage = document.querySelector("#chat-page");
+ var userNameForm = document.querySelector("#usernameForm");
+ var messageForm = document.querySelector("#messageForm");
+ var messageInput = document.querySelector("#message");
+ var messageAreaElement = document.querySelector("#messageArea");
+ var connectingElement = document.querySelector(".connecting");
